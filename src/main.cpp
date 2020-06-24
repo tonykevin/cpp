5bb682ced1d64f6d4f95004d44bdac5a33c3bec2
tonykevin <hi@tonykevin.com>
2020-06-23 20:25:09 -0500

    define data user

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,29 @@
 using namespace std;
 
 int main() {
-  char name[32];
+  // Define data types for user
+  char name[20] = {""};
+  int age= 0;
+  float weight = 0;
+  double profits= 0;
 
-  cout<<"Your name: ";
-  cin>>name;
+  // Get user data
+  cout<<" 💻 User Sign up\n";
+  cout<<" ───────────────\n";
+  cout<<" 🔰 Name    : ";cin>>name;
+  cout<<" 🔰 Age     : ";cin>>age;
+  cout<<" 🔰 Weight  : ";cin>>weight;
+  cout<<" 🔰 Profits : ";cin>>profits;
 
-  cout<<"✨ Hi "<<name<<"\n";
+  // Show info
+  cout<<"\n\n";
+  cout<<" 📚 User profile\n";
+  cout<<" ───────────────\n";
+  cout<<" 👔 name    : "<< name    <<"\n";
+  cout<<" 🌲 age     : "<< age     <<"\n";
+  cout<<" 📋 weight  : "<< weight  <<"\n";
+  cout<<" 💰 profits : "<< profits <<"\n";
+  cout<<"\n\n";
 
   return 0;
 }
